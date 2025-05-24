878def862a37cff44f1b5ecf97c39c0e2520fd4d
grotaclas <grotaclas@gmail.com>
2025-05-24 21:31:04 +0200

    add a helper file to quickly get info about all the used keys in paradox script files in a given folder

---
+++ common/helper.py
@@ -0,0 +1,56 @@
+import collections.abc
+
+
+class Helper:
+
+    def get_data(self, folder):
+        raise NotImplementedError('Subclasses must implement this function')
+
+    def find_all_keys_in_folder(self, folder, depth=0, ignored_toplevel_keys=None):
+        keys = {}
+        if ignored_toplevel_keys is None:
+            ignored_toplevel_keys = []
+        for filename, filedata in self.get_data(folder):
+            for toplevelkey, data in filedata:
+                if toplevelkey in ignored_toplevel_keys:
+                    continue
+                if depth == 0:
+                    self._update_keys_from_data(data, keys)
+                else:
+                    for n2, d2 in data:
+                        if depth == 1:
+                            self._update_keys_from_data(d2, keys)
+                        else:
+                            for n3, d3 in d2:
+                                if depth == 2:
+                                    self._update_keys_from_data(d3, keys)
+                                else:
+                                    for n4, d4 in d3:
+                                        self._update_keys_from_data(d4, keys)
+        print('==Examples==')
+        for key, values in sorted(keys.items()):
+            print('{}: {}'.format(key, list(values)[:3]))
+        print('==Definitions==')
+        for key, values in sorted(keys.items()):
+            value_types = {v if isinstance(v, type) else type(v) for v in values}
+            if len(value_types) == 1:
+                print('{}: {}'.format(key, value_types.pop().__name__))
+            else:
+                print('{}: any  # possible types: {}'.format(key, value_types))
+
+        return keys
+
+    def get_value_for_example(self, value):
+        if isinstance(value, collections.abc.Hashable):
+            return value
+        else:
+            return type(value)
+    def _update_keys_from_data(self, data, keys):
+        if isinstance(data, list):
+            for item in data:
+                self._update_keys_from_data(item, keys)
+        else:
+            for k, v in data:
+                if k not in keys:
+                    keys[k] = set()
+                keys[k].add(self.get_value_for_example(v))
