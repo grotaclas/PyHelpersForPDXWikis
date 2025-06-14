ed27586c278bb43f7b4a7b71523f1499b1000a52
grotaclas <grotaclas@gmail.com>
2025-06-14 13:13:10 +0200

    add option to ignore keys in the Helper

--- common/helper.py
+++ common/helper.py
@@ -6,27 +6,29 @@ class Helper:
     def get_data(self, folder):
         raise NotImplementedError('Subclasses must implement this function')
 
-    def find_all_keys_in_folder(self, folder, depth=0, ignored_toplevel_keys=None):
+    def find_all_keys_in_folder(self, folder, depth=0, ignored_toplevel_keys: list=None, ignored_keys: list=None):
         keys = {}
         if ignored_toplevel_keys is None:
             ignored_toplevel_keys = []
+        if ignored_keys is None:
+            ignored_keys = []
         for filename, filedata in self.get_data(folder):
             for toplevelkey, data in filedata:
                 if toplevelkey in ignored_toplevel_keys:
                     continue
                 if depth == 0:
-                    self._update_keys_from_data(data, keys)
+                    self._update_keys_from_data(data, keys, ignored_keys)
                 else:
                     for n2, d2 in data:
                         if depth == 1:
-                            self._update_keys_from_data(d2, keys)
+                            self._update_keys_from_data(d2, keys, ignored_keys)
                         else:
                             for n3, d3 in d2:
                                 if depth == 2:
-                                    self._update_keys_from_data(d3, keys)
+                                    self._update_keys_from_data(d3, keys, ignored_keys)
                                 else:
                                     for n4, d4 in d3:
-                                        self._update_keys_from_data(d4, keys)
+                                        self._update_keys_from_data(d4, keys, ignored_keys)
         print('==Examples==')
         for key, values in sorted(keys.items()):
             print('{}: {}'.format(key, list(values)[:4]))
@@ -45,12 +47,14 @@ class Helper:
             return value
         else:
             return type(value)
-    def _update_keys_from_data(self, data, keys):
+    def _update_keys_from_data(self, data, keys, ignored_keys: list):
         if isinstance(data, list):
             for item in data:
-                self._update_keys_from_data(item, keys)
+                self._update_keys_from_data(item, keys, ignored_keys)
         else:
             for k, v in data:
+                if k in ignored_keys:
+                    continue
                 if k not in keys:
                     keys[k] = set()
                 keys[k].add(self.get_value_for_example(v))
