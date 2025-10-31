4982a89b959c79cbe798aa6a2217149446a96e81
grotaclas <grotaclas@gmail.com>
2025-10-31 19:38:39 +0100

    eu5: add helper functions to file generator

--- eu5/eu5_file_generator.py
+++ eu5/eu5_file_generator.py
@@ -1,6 +1,9 @@
+from functools import cached_property
+
 from common.file_generator import FileGenerator
 from eu5.game import eu5game
 from eu5.parser import Eu5Parser
+from eu5.text_formatter import Eu5WikiTextFormatter
 
 
 class Eu5FileGenerator(FileGenerator):
@@ -9,3 +12,10 @@ class Eu5FileGenerator(FileGenerator):
 
     def __init__(self):
         super().__init__(eu5game)
+
+    def localize(self, key: str, default: str = None) -> str:
+        return self.parser.localize(key, default)
+
+    @cached_property
+    def formatter(self) -> Eu5WikiTextFormatter:
+        return self.parser.formatter
