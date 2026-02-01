28216771545a53b15fea16f01ec5b902367d1e35
grotaclas <grotaclas@gmail.com>
2026-02-01 23:50:51 +0100

    eu5: one-time script for eu5 modifier counts

--- eu5/generate_one_time_data.py
+++ eu5/generate_one_time_data.py
@@ -3,7 +3,10 @@
 generates list and other things which are not run regularly, but which might come in handy again
 
 """
+import itertools
+import re
 import sys
+from collections import Counter
 
 from eu5.eu5_file_generator import Eu5FileGenerator
 
@@ -21,6 +24,48 @@ class OneTimeGenerator(Eu5FileGenerator):
             country.display_name
             for country in self.parser.countries.values()
             if country.has_flag('supports_southern_court')])
+
+    def generate_modifier_counts(self):
+        ignored_paths = [
+            'in_game/common/*localization*',
+            'in_game/common/attribute_columns',
+            'in_game/common/ethnicities',
+            'in_game/common/genes',
+            'main_menu/common/modifier_icons',
+            'main_menu/common/modifier_type_definitions',
+            'main_menu/common/game_concepts',
+        ]
+        regex_lines = ['|'.join(batch) for batch in itertools.batched([m.name for m in self.parser.modifier_types.values()], 100)]
+        regexes = [re.compile(r'\b(' + regex + r')\b') for regex in regex_lines]
+        modifier_count = Counter()
+        for textfile in self.game.game_path.glob('game/*/common/**/*.txt'):
+            ignored = False
+            for ignored_path in ignored_paths:
+                if ignored_path in str(textfile) or textfile.parent.match(str(self.game.game_path / 'game') + '/' + ignored_path):
+                    ignored = True
+                    break
+            if ignored:
+                continue
+            contents = textfile.read_text(encoding='utf-8_sig')
+            for regex in regexes:
+                modifier_count.update(regex.findall(contents))
+
+        table_data = []
+        for modifier in self.parser.modifier_types.values():
+            if modifier.name in modifier_count:
+                count = modifier_count[modifier.name]
+            else:
+                count = 0
+            table_data.append({
+                'Uses': count,
+                'Name': modifier.name,
+                'Category': modifier.category,
+                'into_template': f'<code><nowiki>{{{{modifier info|{modifier.name}}}}}</nowiki></code>',
+                'Display Name': modifier.display_name,
+                'Description': modifier.description
+            })
+        return self.make_wiki_table(table_data)
+
 
 if __name__ == '__main__':
     OneTimeGenerator().run(sys.argv)
