ddde6ee5a497d87336df2bc8e99b8606ee77ce80
grotaclas <grotaclas@gmail.com>
2026-05-15 14:39:45 +0200

    eu5: add version numbers and checksums for 1.2.2 and 1.2.3

--- eu5/game.py
+++ eu5/game.py
@@ -48,7 +48,9 @@ class EuropaUniversalisV(Game):
                 'dc27a2531a681cb9f00ea1f0dd7d885840c68c46': '1.1.9',
                 'bb626854c1e562fddec318a5a433fb1e4840edf1': '1.1.10',
                 'd4ac783b726c725e30128d9fc2cda2d8ff382c44': '1.2.0',
-                '84ef593274f81edadcf87e0acd86da157ea11a47': '1.2.1'
+                '84ef593274f81edadcf87e0acd86da157ea11a47': '1.2.1',
+                'c85ad4c466c1d832293751c7ce21df98f1a7c0d2': '1.2.2',
+                'a6fa0cc8ffdf63f99aa78c380b6e85691851bbc8': '1.2.3',
             }
             if self.revision in version_by_rev:
                 return version_by_rev[self.revision]
@@ -72,6 +74,8 @@ class EuropaUniversalisV(Game):
         checksum_by_rev = {
             'd4ac783b726c725e30128d9fc2cda2d8ff382c44': '5be7', # 1.2.0
             '84ef593274f81edadcf87e0acd86da157ea11a47': 'e429', # 1.2.1
+            'c85ad4c466c1d832293751c7ce21df98f1a7c0d2': 'fb04', # 1.2.2
+            'a6fa0cc8ffdf63f99aa78c380b6e85691851bbc8': '6a4a', # 1.2.3
         }
         if self.revision in checksum_by_rev:
             return checksum_by_rev[self.revision]
